28095fa91926e13416c3adfe0d5fdfee561cf2a0
Jannis Teunissen <jannis@teunissen.net>
2025-09-01 10:57:07 +0200

    Remove hard coded path from script

--- input/transport_data/script/compute_all.sh
+++ input/transport_data/script/compute_all.sh
@@ -2,12 +2,10 @@
 
 set -e
 
-bolsig=~/opt/sw/bolsigminus/bolsigminus
-
 python generate_all.py
 
 for f in bolsig_script_*.txt; do
-    echo " " | $bolsig "$f"
+    echo " " | bolsigminus "$f"
 done
 
 for f in bolsig_result_*.txt; do
